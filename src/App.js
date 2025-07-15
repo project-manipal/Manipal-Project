7b9f298956de1a37c4be705d6a224c99d9ad0f8b
surya <suryaffa648@gmail.com>
2025-07-15 13:27:35 +0530

    MFLP-16 divakar footer completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Navbar</h1>
+     <p>navbar component will be there</p>
+    </div>
+  );
+}
+
+export default App;
