b3bb522cacb2004ee5adaa33f56de97ecfb0e517
Y.Divakar <ydivakar5b5@gmail.com>
2025-07-19 23:08:21 +0530

    i updated the footer page linking with anchor tag

--- src/App.js
+++ src/App.js
@@ -20,33 +20,6 @@ function App() {
     </Router>
     );
   }
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import FooterAdmin from './Components/Navbar/Footer/FooterAdmin.jsx'
-import FooterDoctor from './Components/Navbar/Footer/FooterDoctor.jsx';
-import FooterPatient from './Components/Navbar/Footer/FooterPatient.jsx';
-import FooterGuest from './Components/Navbar/Footer/FooterGuest.jsx';
 
-function App() {
-  return (
-    <div className="App">
-
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={
-          <FooterGuest/>  
-        }/>
-                <Route path='/FooterGuest/*' element={<FooterGuest/>}></Route>
-
-        <Route path='/FooterAdmin/*' element={<FooterAdmin/>}></Route>
-                <Route path='/FooterDoctor/*' element={<FooterDoctor/>}></Route>
-        <Route path='/FooterPatient/*' element={<FooterPatient/>}></Route>
-
-
-      </Routes>
-      </BrowserRouter>
-     
-    </div>
-  );
-}
 
 export default App;
