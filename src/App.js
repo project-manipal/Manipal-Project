c5c8c8205f8201a05eacdff55196cf8d9b870513
Y.Divakar <ydivakar5b5@gmail.com>
2025-07-21 12:49:41 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
-import './App.css';
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import GuestApp from './Layouts/GuestLayouts/GuestLayout.jsx'
-import AdminApp from './Layouts/AdminLayouts/AdminLayout.jsx'
 
-function App() {
-  return (
-    <div className="App">
-
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={
-          <GuestApp/>  
-        }/>
-        <Route path='/admin/*' element={<AdminApp/>}/>
-      </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
 
 import './App.css';
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
