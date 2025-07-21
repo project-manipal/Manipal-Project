8636e23f5204b90a719daee14330b4dd36bc6954
Y.Divakar <ydivakar5b5@gmail.com>
2025-07-21 12:44:52 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes, Route, BrowserRouter } from 'react-router-dom';
 import FooterGuest from './Components/Navbar/Footer/FooterGuest';
 import FooterDoctor from './Components/Navbar/Footer/FooterAdmin';
 import FooterAdmin from './Components/Navbar/Footer/FooterDoctor';
 import FooterPatient from './Components/Navbar/Footer/FooterPatient';
+import GuestApp from './Layouts/GuestLayouts/GuestLayout.jsx';
+import AdminApp from './Layouts/AdminLayouts/AdminLayout.jsx';
+
 function App() {
   return (
-    <Router>
+    
       <div className="App">
+        <BrowserRouter>
+     
         <Routes>
+          <Route path="/" element={<GuestApp />} />
+          <Route path="/admin/*" element={<AdminApp />} />
           <Route path="/FooterDoctor" element={<FooterDoctor />} />
           <Route path="/FooterAdmin" element={<FooterAdmin />} />
           <Route path="/FooterPatient" element={<FooterPatient />} />
-          <Route path="/" element={<FooterGuest />} />
+          <Route path="/FooterGuest" element={<FooterGuest />} />
+          
         </Routes>
-      </div>
+      
+   
+    </BrowserRouter>
+
+  
       <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-      </div>
-    </Router>
+      </div> 
+    </div>
     );
   }
 
